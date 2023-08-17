9493b279e54379bdb48461524796512887f633d8
Bidhu1024 <bidhu1024av@gmail.com>
2023-08-17 10:33:46 +0530

    Learnt about server routing, request and response

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const http = require('http')
 
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url)
+    console.log(req)
+    if(req.url === "/about"){
+        res.end("<h1>This is about page</h1>")
+
+    }else if(req.url === "/"){
+        res.end("<h1>This is home page</h1>")
+    }else{
+        res.end("<h1>Page not found 404</h1>")
+    }
 })
 
 server.listen(5000, ()=>{
