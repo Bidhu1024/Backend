f5268d0cd9d02952f00a307056b0dfc1a1b8febf
Bidhu1024 <bidhu1024av@gmail.com>
2023-08-25 00:43:47 +0530

    sss

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-import http from "http"
-import name from './features.js'
-import love from "./LoveCalc.js";
-import fs from "fs"
 
-const server = http.createServer((req,res)=>{
-    console.log(req)
-    if(req.url === "/about"){
-        res.end("<h1>This is about page</h1>")
+import express from "express"
 
-    }else if(req.url === "/"){
-        fs.readFile("./index.html", (err,data)=>{
-        console.log(err)
-        res.end(data)
-        })
-       
-    }else{
-        res.end("<h1>Page not found 404</h1>")
-    }
+const app = express()
+app.get("/",(req,res)=>{
+    res.sendStatus(500)
 })
-
-server.listen(5000, ()=>{
-    console.log(`listening on port 5000`)
+app.listen(5000,()=>{
+    console.log("Server is running")
 })
