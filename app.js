a96da3e10ee4f510c5d136cbd7549a1259b84339
Bidhu1024 <bidhu1024av@gmail.com>
2023-08-25 00:49:03 +0530

    json

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import express from "express"
 
 const app = express()
 app.get("/",(req,res)=>{
-    res.sendStatus(500)
+    res.json({//sending json
+"name":"hehehehe",
+    })
 })
 app.listen(5000,()=>{
     console.log("Server is running")
